6f8a03efd76d6f4f95a38f1c438e90860dff8e07
Sakeena402 <sakeenaf315@gmail.com>
2024-11-15 00:43:40 +0500

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Hero from "@/components/Hero";
+
+
+export default function Home() {
+  return (
+   <main className="relative justify-center items-center bg-black-100 flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <div className="max-w-7xl w-full">
+       
+      <Hero/>
+    </div>
+
+   </main>
+   );
+}
